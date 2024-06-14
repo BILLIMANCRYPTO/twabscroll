968f5128381860b6dafe81c94967c6c4e6a5dede
BnbBillionaire <99882254+BILLIMANCRYPTO@users.noreply.github.com>
2024-06-14 21:35:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ from queue import Queue
 
 # RPC узлы
 RPC_URLS = [
-    'https://scroll-mainnet.blastapi.io/a03e175a-af6f-478f-ac6d-84c22e26df64',
-    'https://scroll-mainnet.blastapi.io/9d146085-0f72-498a-8c0e-fc8334e9ee26',
-    'https://scroll-mainnet.blastapi.io/184f19f8-2633-4a67-9c8a-39a95d058895',
-    'https://scroll-mainnet.blastapi.io/658014c3-6d8e-4321-876c-ccae99f2e922'
+    'твоя rpc',
+    'твоя rpc',
+    'твоя rpc',
+    'твоя rpc'
+#'твоя rpc' - можешь добавить сколько угодно
 ]
 
 # Инициализация Web3
